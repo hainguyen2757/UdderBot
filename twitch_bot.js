3b6a503073803c1822a328356256e57ab5020822
hainguyen2757 <thanhhai2757@gmail.com>
2023-08-08 17:13:50 +0700

    1.0.1
    Change bot name to user name
    added auto fight accept
    added logging incoming messages
    figure out incoming message is lower case sensitive

--- twitch_bot.js
+++ twitch_bot.js
@@ -22,19 +22,43 @@ const client1 = new tmi.Client(bot1Config);
 const client2 = new tmi.Client(bot2Config);
 const randomDelay = Math.random() * 200 + 1000; //delay between 1 and 2.5 seconds
 
-//client1.connect();
-//console.log("Bot 1 connected to channel:", bot1Config.channels[0]);
+client1.connect();
+console.log("Fathai95 connected to channel:", bot1Config.channels[0]);
 
 client2.connect();
-console.log("Bot 2 connected to channel:", bot2Config.channels[0]);
+console.log("uddertastic connected to channel:", bot2Config.channels[0]);
 
-client1.on("message", (channel, userstate, message, self) => {
+client1.on("message", async (channel, userstate, message, self) => {
   //fathai95
   if (self) return; // Ignore messages from the bot itself
-
+  //console.log(`Incoming message- ${userstate.username}: ${message}`);
+  //console.log(`Message includes "udder": ${message.toLowerCase().includes("udder")}`);
+  //console.log(`Message includes "!give uddertastic": ${message.toLowerCase().includes("!give uddertastic")}`);
+  //console.log(`Message starts with "!take uddertastic": ${message.toLowerCase().startsWith("!take uddertastic")}`);
+  //console.log(`Message includes "has defeated Fathai95": ${message.toLowerCase().includes("fathai95 has defeated")}`);
+  //console.log(`Message includes "has defeated Fathai95": ${message.toLowerCase().includes("has defeated fathai95")}`);
   if (message.includes("The minions are out!")) {
-    console.log(`Bot 1: Responding to ${userstate.username} in ${channel}`);
+    console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
     client1.say(channel, `!hunt all`);
+  }
+  if (message.includes("!fight Fathai95")) {
+    await delay(1000);
+    if (message.includes("There is already a fight going on")) {
+      return;
+    } else {
+      console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
+      await delay(1000);
+      client1.say(channel, `!fight`);
+    }
+  }
+  if (message.toLowerCase().includes("Fathai95 has defeated")) {
+    console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
+    await delay(200);
+    client1.say(channel, `EZ Clap`);
+  }
+  if (message.toLowerCase().includes("has defeated Fathai95")) {
+    console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
+    client1.say(channel, `NOOO`);
   }
 });
 
@@ -42,13 +66,13 @@ let lastResponseIndex = -1; // Initialize with an invalid index
 let currentResponseIndex = -1; // Initialize with an invalid index
 
 const responses = [
-    "gachiBASS Boner CUm",
-    "boobahri boobaCheck boobaPls",
-    "BOOBAL BERSERK BOOBAR",
-    "BOOBAL CUm boobaPls",
-    "hyperPOGGIES POGGIES hyperPOGGIES POGGIES",
-    "FeelsCrazyMan Boner CUm lickR",
-  ];
+  "gachiBASS Boner CUm",
+  "boobahri boobaCheck boobaPls",
+  "BOOBAL BERSERK BOOBAR",
+  "BOOBAL CUm boobaPls",
+  "hyperPOGGIES POGGIES hyperPOGGIES POGGIES",
+  "FeelsCrazyMan Boner CUm lickR",
+];
 
 function getRandomResponseIndex() {
   let randomIndex = Math.floor(Math.random() * responses.length);
@@ -64,99 +88,52 @@ function getRandomResponseIndex() {
   return randomIndex;
 }
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-
-// client2.on("message", async (channel, userstate, message, self) => {
-//   //udder
-//   const username = userstate.username;
-
-//   if (self) return; // Ignore messages from the bot itself
-//   //const lowercaseMessage = message.toLowerCase();
-
-//   if (message.includes("The minions are out!")) {
-//     console.log(`Bot 2: Responding to ${userstate.username} in ${channel}`);
-//     setTimeout(() => {
-//       client2.say(channel, `!hunt half`);
-//     }, randomDelay);
-//   }
-
-//   const randomResponseIndex = getRandomResponseIndex();
-//     currentResponseIndex = randomResponseIndex;
-//     const selectedResponse = responses[randomResponseIndex];
-//    if (message === "udder") {
-//     // if(message.includes("udder")){
-//     console.log(
-//       `Bot 2: Responding to ${userstate.username} in ${channel} with ${selectedResponse} in ${randomDelay}}`
-//     );
-//     await delay(randomDelay);
-//     client2.say(channel, `${selectedResponse}`);
-
-//   }
-
-//   if (message.includes("!give uddertastic")) {
-//     // Choose a random response from the array
-//     console.log(`Bot 2: Responding to ${userstate.username} in ${channel}`);
-//     await delay(randomDelay);
-//     client2.say(channel, `Thanks for the minions @${userstate.username}`);
-    
-//   }
-//   if (message.startsWith("!take uddertastic") && username === "fathai95") {
-//     // Choose a random response from the array
-//     // Extract the number from the message
-//     const match = message.match(/\d+/);
-//     const number = match ? parseInt(match[0]) : 0;
-//     console.log(`udder: giving ${userstate.username} ${number} points`);
-//     await delay(randomDelay);
-//     client2.say(channel, `!give ${userstate.username} ${number}`);
-//   }
-
-//   //if stream end, end bot
-// });
-
-let currentResponse = ''; // Initialize with an empty string
+let currentResponse = ""; // Initialize with an empty string
 
 client2.on("message", async (channel, userstate, message, self) => {
-    // udder
-    const username = userstate.username;
-  
-    if (self) return; // Ignore messages from the bot itself
-  
-    if (message.includes("The minions are out!")) {
-      console.log(`Bot 2: Responding to ${userstate.username} in ${channel}`);
-      setTimeout(() => {
-        client2.say(channel, `!hunt half`);
-      }, randomDelay);
+  // udder
+  const username = userstate.username;
+
+  if (self) return; // Ignore messages from the bot itself
+
+  if (message.includes("The minions are out!")) {
+    console.log(`uddertastic: Responding to ${userstate.username} in ${channel}`);
+    setTimeout(() => {
+      client2.say(channel, `!hunt half`);
+    }, randomDelay);
+  }
+
+  if (message === "udder") {
+    let randomResponseIndex = getRandomResponseIndex();
+    while (responses[randomResponseIndex] === currentResponse) {
+      randomResponseIndex = getRandomResponseIndex(); // Choose a new response if it's the same as the current one
     }
-  
-    if (message.includes("udder")) {
-        let randomResponseIndex = getRandomResponseIndex();
-        while (responses[randomResponseIndex] === currentResponse) {
-          randomResponseIndex = getRandomResponseIndex(); // Choose a new response if it's the same as the current one
-        }
-        currentResponse = responses[randomResponseIndex];
-      console.log(`Bot 2: Responding to ${userstate.username} in ${channel} with ${currentResponse} after ${randomDelay}`);
-  
-      await delay(randomDelay);
-      client2.say(channel, `${currentResponse}`);
-    }
-  
-    if (message.includes("!give uddertastic")) {
-      console.log(`Bot 2: Responding to ${userstate.username} in ${channel}`);
-      await delay(randomDelay);
-      client2.say(channel, `Thanks for the minions @${userstate.username}`);
-    }
-  
-    if (message.startsWith("!take uddertastic") && username === "fathai95") {
-      const match = message.match(/\d+/);
-      const number = match ? parseInt(match[0]) : 0;
-      console.log(`udder: giving ${userstate.username} ${number} points`);
-      await delay(randomDelay);
-      client2.say(channel, `!give ${userstate.username} ${number}`);
-    }
-  });
-  
+    currentResponse = responses[randomResponseIndex];
+    console.log(
+      `uddertastic: Responding to ${userstate.username} in ${channel} with ${currentResponse} after ${randomDelay}`
+    );
+
+    await delay(randomDelay);
+    client2.say(channel, `${currentResponse}`);
+  }
+
+  if (message.includes("!give uddertastic")) {
+    console.log(`uddertastic: Responding to ${userstate.username} in ${channel}`);
+    await delay(randomDelay);
+    client2.say(channel, `Thanks for the minions @${userstate.username}`);
+  }
+
+  if (message.startsWith("!take uddertastic") && username === "fathai95") {
+    const match = message.match(/\d+/);
+    const number = match ? parseInt(match[0]) : 0;
+    console.log(`udder: giving ${userstate.username} ${number} points`);
+    await delay(randomDelay);
+    client2.say(channel, `!give ${userstate.username} ${number}`);
+  }
+});
 
 client1.on("stream-down", (channel) => {
   console.log(`Stream has ended in ${channel}. Stopping the bot.`);
