228444f0db4c9199db55ceb352b6a4dc163ae3af
hainguyen2757 <thanhhai2757@gmail.com>
2023-08-12 20:57:58 +0700

    1.0.3
    add rock paper scissor betting to uddertastic
    clean up some code
    changed fighting promps

---
+++ boss.js
@@ -0,0 +1,45 @@
+// REMOVE THE & SYMBOL IN THE EMOTES :&classical_building: :&moai: :&mount_fuji: :&tokyo_tower:
+// introduction: "The ground trembles as Terragor, the Earthshaker, awakens from its slumber. This colossal golem :&moai: towers over the arena, its very steps causing quakes. Your minions muster their courage, preparing to face this monumental challenge.",
+//   bossDefeatText: "The might of Terragor :&moai: proves insurmountable. The arena is left in ruins panicBasket panicBasket panicBasket , with the golem standing tall amidst the devastation.",
+//   bossVictoryText: "With a final echoing thud, Terragor :&moai: crumbles and returns to the earth from whence it came. The arena stands still, :&classical_building: recovering from the cataclysmic battle .",
+//   attacks: [
+//     {
+//       text: "Terragor :&moai: raises its arm, smashing it down with devastating force, aiming to flatten groups of minions!",
+//       successChance: 88,
+//       successReward: 10,
+//       failPenalty: 60,
+//       successText: "Chat's minions scatter in the nick of time, CoolStoryBob narrowly avoiding the crushing blow!",
+//       failText: "Several of Chat's minions are caught in the shockwave, KAPOW sent flying from the impact!",
+//       attackInterval: 5000,
+//       resultDelayInterval: 5000
+//     },
+//     {
+//       text: "With a powerful stomp, Terragor :&moai: causes the ground to rupture, opening up chasms beneath the minions!",
+//       successChance: 85,
+//       successReward: 10,
+//       failPenalty: 60,
+//       successText: "Chat's minions leap over the fractures, Kappa FBBlock using the terrain to their advantage!",
+//       failText: "Unfortunate minions plummet into the depths, :skull: lost to the earth's embrace!",
+//       attackInterval: 5500,
+//       resultDelayInterval: 5500
+//     },
+//     {
+//         text: "Terragor :&moai: grabs a massive boulder :&mount_fuji: and hurls it towards the minions, threatening to obliterate everything in its path!",
+//         successChance: 80,
+//         successReward: 10,
+//         failPenalty: 65,
+//         successText: "Chat's minions dodge and weave, EleGiggle evading the incoming projectile with agility!",
+//         failText: "The boulder crashes down, :&mount_fuji: KAPOW crushing any minion unlucky enough to be in its trajectory!",
+//         attackInterval: 7000,
+//         resultDelayInterval: 7000
+//       },
+//       {
+//         text: "The golem :&moai: channels its innate power, causing pillars of stone :&tokyo_tower: :&tokyo_tower: to jut out and imprison groups of minions!",
+//         successChance: 65,
+//         successReward: 80,
+//         failPenalty: 70,
+//         successText: "Chat's minions break free from their stony prisons, SwiftRage turning the tide against Terragor!",
+//         failText: "Several minions are trapped, BibleThump struggling to free themselves from the earthen grasp!",
+//         attackInterval: 15000,
+//         resultDelayInterval: 15000
+//       }
--- twitch_bot.js
+++ twitch_bot.js
@@ -20,10 +20,10 @@ const bot2Config = {
 
 const client1 = new tmi.Client(bot1Config);
 const client2 = new tmi.Client(bot2Config);
-const randomDelay = Math.random() * 1100 + 200; //delay between 200ms and 1300ms seconds
-var Bot1Hunt=1;
-var Bot1Fight=1;
-
+const randomDelay = Math.random() * 1700 + 200; //delay between 200ms and 1300ms seconds
+var Bot1Hunt = 1;
+var Bot1Fight = 1;
+let udderBet=0;
 function getRandomDelay() {
   return Math.random() * 1100 + 200; // Generate random delay between 200ms and 1300ms
 }
@@ -32,107 +32,133 @@ console.log("Fathai95 connected to channel:", bot1Config.channels[0]);
 
 client2.connect();
 console.log("uddertastic connected to channel:", bot2Config.channels[0]);
-
+console.log("Hunt is on || fighting is on || Udder bet is off");
 client1.on("message", async (channel, userstate, message, self) => {
   //fathai95
   const username = userstate.username;
+  const capturePattern = /(\S+) captured/i;
+  //const captureTextPattern = /captured (\S+) fathai95/i;
+
   if (self) return; // Ignore messages from the bot itself
   console.log(`Incoming message- ${userstate.username}: ${message}`);
   //console.log(`Message includes "udder": ${message.toLowerCase().includes("udder")}`);
   //console.log(`Message includes "!give uddertastic": ${message.toLowerCase().includes("!give uddertastic")}`);
-  //console.log(`Message starts with "!take uddertastic": ${message.toLowerCase().startsWith("!take uddertastic")}`);
   //console.log(`Message includes "has defeated Fathai95": ${message.toLowerCase().includes("fathai95 has defeated")}`);
-  //console.log(`Message includes "has defeated Fathai95": ${message.toLowerCase().includes("has defeated fathai95")}`);
-  if (message==='!hunting on' && username === "fathai95") {
+  if (message === "!hunting on" && username === "fathai95") {
     Bot1Hunt = 1;
-    console.log('Bot1 hunt is on');
+    console.log("Bot1 hunt is on");
     return;
-  }  else if (message==='!hunting off' && username === "fathai95") {
+  } else if (message === "!hunting off" && username === "fathai95") {
     Bot1Hunt = 0;
-    console.log('Bot1 hunt is off');
+    console.log("Bot1 hunt is off");
     return;
   }
-  if (message==='!fighting on' && username === "fathai95") {
+  if (message === "!fighting on" && username === "fathai95") {
     Bot1Fight = 1;
-    console.log('Bot1 fight is on');
+    console.log("Bot1 fight is on");
     return;
-  }  else if (message==='!fighting off' && username === "fathai95") {
+  } else if (message === "!fighting off" && username === "fathai95") {
     Bot1Fight = 0;
-    console.log('Bot1 fight is off');
+    console.log("Bot1 fight is off");
     return;
   }
 
-  if (message.includes("The minions are out!")&& Bot1Hunt===1) {
+  if (
+    message.includes("Type !hunt <amount>") &&
+    Bot1Hunt === 1 &&
+    username === "chatsrpg"
+  ) {
     console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
+    await delay(randomDelay);
     client1.say(channel, `!hunt all`);
   }
-  if (message.includes('@Fathai95 (+')) {
 
+  if (
+    message.includes("!boss <amount> to get started") &&
+    username === "chatsrpg" &&
+    Bot1Hunt === 1
+  ) {
+    //var minionCount = 0;
+    console.log(`FatHai95 hunting the boss BOSS`);
+    await delay(1000);
+    client1.say(channel, `!boss all`);
+  }
+  const responsesWin = [
+    "LETSGOOO Winions",
+    "EZ Clap winions",
+    "BERSERK GIMME MY MINIONS",
+    "Pog winions",
+    "gachiHYPER HYPERCLAP winions",
+    "Ok minions",
+    "pepePoint",
+    "Igotmyeyesonyou",
+  ];
+  const responsesLoss = [
+    "NOOO my minions",
+    "NotLikeThis",
+    "BERSERK GIMME BACK MY MINIONS",
+    "NotCrying my minions",
+    "BabyRage minions",
+    "NOOO dont leave me",
+    "Madge i'll get you for this",
+  ];
+  const randomResponseIndex = Math.floor(Math.random() * responsesWin.length);
+  const randomLossResponseIndex = Math.floor(
+    Math.random() * responsesLoss.length
+  );
+  const responseWin = responsesWin[randomResponseIndex];
+  const responseLoss = responsesLoss[randomLossResponseIndex];
+
+  if (message.includes("@fathai95 (+")) {
     console.log(`FatHai95 is celebrating the hunt`);
     await delay(200);
-    client1.say(channel, `gachiHYPER HYPERCLAP`);
-  }else if (message.includes("@Fathai95 (-")) {
+    client1.say(channel, responseWin);
+  } else if (message.includes("@fathai95 (-")) {
     console.log(`FatHai95 is mourning the hunt`);
     await delay(200);
-    client1.say(channel, `NOOO my minions`);
+    client1.say(channel, responseLoss);
+  }
+  function sanitizeMessage(msg) {
+    // Replace non-printable and non-ASCII characters
+    return msg
+      .replace(/[^ -~]+/g, "")
+      .trim()
+      .toLowerCase();
   }
 
-
-  
-  if (message.includes("!fight fathai95")&& Bot1Fight===1) {
+  if (
+    message.toLowerCase().includes("has challenged @Fathai95") ||
+    message.includes("has challenged Fathai95") ||
+    (message.includes("has challenged fathai95") && Bot1Fight === 1)
+  ) {
     console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
     await delay(1000);
     client1.say(channel, `!fight`);
   }
-  if (message.toLowerCase().includes("fathai95 has defeated")) {
-
+  if (message.toLowerCase().includes("fathai95 captured")) {
     console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
-    const randomChoice = Math.random();
-    let response;
-    if (randomChoice <0.3) {
-      response = 'EZ Clap';
-    }else if(0.3< randomChoice <0.6){
-      response = 'LOser';
-    }
-    else{
-      response = 'BERSERK GIMME YOUR MINIONS';
-    }
     await delay(200);
-    client1.say(channel, response);
+    client1.say(channel, responseWin);
   }
-  if (message.toLowerCase().includes("has defeated fathai95")) {
-    console.log(`FatHai95: Responding to ${userstate.username} in ${channel}`);
-  const randomChoice = Math.random();
-    let response;
-    if (randomChoice <0.3) {
-      response = 'RAGEY';
-    }else if(0.3< randomChoice <0.6){
-      response = 'NOOO my minions';
+  if (capturePattern.test(message)) {
+    const match = message.match(capturePattern);
+    const capturedUser = match[1]; // Extract the captured minions number
+    //console.log(`Bot 1: Captured ${capturedUser} minions by ${userstate.username} in ${channel}`);
+    // Your response or action logic here
+    //console.log('pattern '+capturePattern);
+    console.log(capturePattern.test(message));
+    console.log("user won is " + capturedUser);
+    if (
+      capturedUser !== "@fathai95" &&
+      message.toLowerCase().includes("@fathai95")
+    ) {
+      await delay(200);
+      console.log(`FatHai95: Responding ${responseLoss} to ${capturedUser}`);
+      client1.say(channel, responseLoss);
     }
-    else{
-      response = 'NotCrying';
-    }
-    await delay(200);
-    client1.say(channel, response);
   }
 });
-
-let lastResponseIndex = -1; // Initialize with an invalid index
-let currentResponseIndex = -1; // Initialize with an invalid index
-let lastResponse = ''; // Initialize with an empty string
-// function getRandomResponseIndex() {
-//   let randomIndex = Math.floor(Math.random() * responses.length);
-
-//   // Ensure the new response is different from the previous one
-//   while (
-//     randomIndex === lastResponseIndex ||
-//     randomIndex === currentResponseIndex
-//   ) {
-//     randomIndex = Math.floor(Math.random() * responses.length);
-//   }
-
-//   return randomIndex;
-// }
+let lastResponse = ""; // Initialize with an empty string
 
 const emoteResponses = [
   "gachiBASS Boner CUm",
@@ -149,7 +175,6 @@ function getRandomResponseEmoteIndex() {
   return getRandomResponseIndex(emoteResponses);
 }
 
-
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -159,16 +184,33 @@ let currentResponse = ""; // Initialize with an empty string
 client2.on("message", async (channel, userstate, message, self) => {
   // udder
   const username = userstate.username;
-
+  const lowercaseMessage = message.toLowerCase();
+  const givePattern = /uddertastic give me (\d+)/i;
+  const rpsPattern = /!give uddertastic (\d+) (rock|paper|scissor)/i;
+  const taggedUserPattern = /@?uddertastic\s+(rock|paper|scissor)/i;
+  let playerChoice = null;
+  let gameOutcome = null;
+  //const giveUserPattern = /uddertastic give (\S+)/i;
   if (self) return; // Ignore messages from the bot itself
 
-  if (message.includes("The minions are out!")) {
+  if (message.includes("Type !hunt <amount>") && username === "chatsrpg") {
     console.log(
       `uddertastic: Responding to ${userstate.username} in ${channel}`
     );
+    console.log("randomdelay of !hunt on udder: " + randomDelay);
     setTimeout(() => {
       client2.say(channel, `!hunt half`);
     }, randomDelay);
+  }
+
+  if (
+    message.includes("!boss <amount> to get started") &&
+    username === "chatsrpg"
+  ) {
+    //var minionCount = 0;
+    console.log(`uddertastic hunting the boss BOSS`);
+    await delay(randomDelay);
+    client2.say(channel, `!boss half`);
   }
 
   if (message === "udder") {
@@ -185,37 +227,164 @@ client2.on("message", async (channel, userstate, message, self) => {
     );
 
     await delay(randomDelay);
-    client2.say(channel, `${currentResponse} 2`);
+    client2.say(channel, `${currentResponse}`);
   }
 
-  if (message.includes("!give uddertastic")) {
-    console.log(
-      `uddertastic: Responding to ${userstate.username} in ${channel}`
-    );
-    await delay(randomDelay);
-    client2.say(channel, `Thanks for the minions @${userstate.username}`);
-  }
+  // if (message.includes("!give uddertastic")) {
+  //   console.log(
+  //     `uddertastic: Responding to ${userstate.username} in ${channel}`
+  //   );
+  //   await delay(randomDelay);
+  //   client2.say(channel, `Thanks for the minions @${userstate.username}`);
+  // }
 
-  if (message.startsWith("!take uddertastic") && username === "fathai95") {
-    const match = message.match(/\d+/);
-    const number = match ? parseInt(match[0]) : 0;
-    console.log(`udder: giving ${userstate.username} ${number} points`);
-    await delay(randomDelay);
-    client2.say(channel, `!give ${userstate.username} ${number}`);
-  }
   if (message.startsWith("uddertastic say") && username === "fathai95") {
     const response = message.replace("uddertastic say", "").trim();
-    console.log(`udder: responding - ${response}`);
     await delay(randomDelay);
     client2.say(channel, `${response}`);
   }
+
+  if (givePattern.test(message)) {
+    const match = message.match(givePattern);
+    const giveMinion = match[1]; // Extract the captured minions number
+    //console.log(`Bot 1: Captured ${capturedUser} minions by ${userstate.username} in ${channel}`);
+    // Your response or action logic here
+    //console.log('pattern '+capturePattern);
+    //console.log(message);
+    //console.log("minions " + capturedUser);
+    if (username === "fathai95") {
+      await delay(300);
+      console.log(`uddertastic is giving Fathai95 ${giveMinion} minions`);
+      client2.say(channel, "!give fathai95 " + giveMinion);
+      await delay(2500);
+      client2.say(
+        channel,
+        "Please dont spend it all in one place Igotmyeyesonyou"
+      );
+    }
+  }
+  if (message.includes("!rps help")) {
+    client2.say(
+      channelName,
+      "Type '!rps rock', '!rps paper', or '!rps scissors' to play Rock-Paper-Scissors!"
+    );
+  }
+
+  if (taggedUserPattern.test(message)) {
+    const match = message.match(taggedUserPattern);
+    const taggedUser = userstate.username;
+    const choice = match[1].toLowerCase();
+    const udderChoice = ["rock", "paper", "scissor"];
+    const randomResponseIndex = Math.floor(Math.random() * udderChoice.length);
+    const udderRespond = udderChoice[randomResponseIndex];
+    const rpsReward = 1000;
+    console.log("choice is " + choice);
+
+    if (choice === "rock" || choice === "paper" || choice === "scissor") {
+      playerChoice = choice;
+      // client2.say(channel, udderRespond);
+      // await delay(500);
+      if (udderRespond && playerChoice) {
+        // Calculate the game outcome
+        if (udderRespond === playerChoice) {
+          gameOutcome = "It's a tie!";
+          client2.say(channel, udderRespond + ", " + gameOutcome);
+        } else if (
+          (playerChoice === "rock" && udderRespond === "scissor") ||
+          (playerChoice === "paper" && udderRespond === "rock") ||
+          (playerChoice === "scissor" && udderRespond === "paper")
+        ) {
+          gameOutcome = `${taggedUser} wins!`;
+          client2.say(channel, udderRespond + ", " + gameOutcome);
+        } else {
+          gameOutcome = "Uddertastic wins!";
+          client2.say(channel, udderRespond + ", " + gameOutcome);
+        }
+
+        // Announce the game outcome
+        //client2.say(channel, `${gameOutcome}`);
+        if (gameOutcome.includes(taggedUser)) {
+          await delay(1200);
+          client2.say(channel, `!give ${taggedUser} `+rpsReward);
+        } else if (gameOutcome.includes("Uddertastic")) {
+          return;
+        }
+        // Reset game state
+        //udderChoice = null;
+        playerChoice = null;
+        gameOutcome = null;
+      }
+    }
+  }
+
+  
+if (message ==="!bet on"&&username==="fathai95") {
+  udderBet=1;
+  client2.say(channel,"/me udder is betting! do !give uddertastic <amount> <rock|paper|scissor> you can win x3")
+  console.log("udderbet is "+udderBet);
+  return;
+}else if (message ==="!bet off"&&username==="fathai95") {
+  udderBet=0;
+  client2.say(channel,"/me udder is not betting anymore!")
+  return;
+}
+  if (rpsPattern.test(lowercaseMessage)) {
+    const match = lowercaseMessage.match(rpsPattern);
+    const betAmount = parseInt(match[1]);
+    const maxBetAmount = 10000; // Set the maximum bet amount here
+    const taggedUser = userstate.username;
+    const choice = match[2].toLowerCase();
+    const udderChoice = ["rock", "paper", "scissor"];
+    const randomResponseIndex = Math.floor(Math.random() * udderChoice.length);
+    const udderRespond = udderChoice[randomResponseIndex];
+    console.log(taggedUser+" choice is " + choice+" || bet is "+betAmount+' || bet on? '+udderBet);
+    if(udderBet===0){
+      client2.say(channel,'Udder is not taking bets right now!');
+      return;
+    }
+    
+    if (betAmount > maxBetAmount && udderBet===1) {
+      client2.say(channel, `Sorry ${taggedUser}, the maximum bet amount is ${maxBetAmount}`);
+      return; // Exit the function
+    }
+    await delay(1500);
+    if (choice === "rock" || choice === "paper" || choice === "scissor" && udderBet===1) {
+      playerChoice = choice;
+      if (udderRespond && playerChoice) {
+        // Calculate the game outcome
+        if (udderRespond === playerChoice) {
+          gameOutcome = "It's a tie!";
+          client2.say(channel, udderRespond + ", " + gameOutcome);
+        } else if (
+          (playerChoice === "rock" && udderRespond === "scissor") ||
+          (playerChoice === "paper" && udderRespond === "rock") ||
+          (playerChoice === "scissor" && udderRespond === "paper")
+        ) {
+          gameOutcome = `${taggedUser} wins!`;
+          client2.say(channel, udderRespond + ", " + gameOutcome);
+        } else {
+          gameOutcome = "Uddertastic wins!";
+          client2.say(channel, udderRespond + ", " + gameOutcome);
+        }
+
+        // Announce the game outcome
+        //client2.say(channel, `${gameOutcome}`);
+        if (gameOutcome.includes(taggedUser)) {
+          await delay(1200);
+          client2.say(channel, `!give ${taggedUser} `+ betAmount*3);
+        } else if (gameOutcome.includes("Uddertastic")) {
+          return;
+        }
+        // Reset game state
+        //udderChoice = null;
+        playerChoice = null;
+        gameOutcome = null;
+      }
+    }
+  }
 });
 
-client1.on("stream-down", (channel) => {
-  client1.disconnect();
-  console.log(`Stream has ended in ${channel}. Stopping the bot.`);
-});
-client2.on("stream-down", (channel) => {
-  client2.disconnect();
-  console.log(`Stream has ended in ${channel}. Stopping the bot.`);
-});
+// client2.on("stream-down", (channel) => {
+//   client2.disconnect();
+//   console.log(`Stream has ended in ${channel}. Stopping the bot.`);
+// });
